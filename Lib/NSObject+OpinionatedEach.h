ba9362155dabef92620dd4cdbebda8012eef47a6
Leo Schweizer <leoschweizer@users.noreply.github.com>
2015-08-02 22:59:27 +0200

    comments

--- Lib/NSObject+OpinionatedEach.h
+++ Lib/NSObject+OpinionatedEach.h
@@ -8,8 +8,21 @@ typedef void (^EachSeparatorBlock)(void);
 
 @interface NSObject (OpinionatedEach)
 
+/**
+ * Evaluate eachBlock with each of the receiver's elements as the argument.
+ */
 - (void)each:(EachBlock)eachBlock;
+
+/**
+ * Evaluate eachBlock with each of the receiver's elements as the first argument
+ * and it's index as the second.
+ */
 - (void)eachWithIndex:(EachWithIndexBlock)eachBlock;
+
+/**
+ * Evaluate eachBlock for each element in the collection. Between each pair of 
+ * elements, but not before the first or after the last, evaluate the separatorBlock.
+ */
 - (void)each:(EachBlock)eachBlock separatedBy:(EachSeparatorBlock)separatorBlock;
 
 @end

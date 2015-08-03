45f9963468447441f97accb603b8512c51fcc75b
Leo Schweizer <leoschweizer@users.noreply.github.com>
2015-08-03 15:30:50 +0200

    comment for map:

--- Lib/NSObject+OpinionatedMapping.h
+++ Lib/NSObject+OpinionatedMapping.h
@@ -6,6 +6,11 @@ typedef id (^OCMapBlock)(id each);
 
 @interface NSObject (OpinionatedMapping)
 
+/**
+ * Evaluate mapblock with each of the values of the receiver as the
+ * argument.  Collect the resulting values into a collection that is like
+ * the receiver. Answer the new collection.
+ */
 - (id)map:(OCMapBlock)mapBlock;
 
 @end

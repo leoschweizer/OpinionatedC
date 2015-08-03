a8890e3c5398c4ae312fd53ca6abe6b103feaf71
Leo Schweizer <leoschweizer@users.noreply.github.com>
2015-08-03 15:29:39 +0200

    comments for first / first:

--- Lib/NSObject+OpinionatedSubsetting.h
+++ Lib/NSObject+OpinionatedSubsetting.h
@@ -3,7 +3,15 @@
 
 @interface NSObject (OpinionatedSubsetting)
 
+/**
+ * Answer the first element. If the receiver is empty, answer nil.
+ */
 - (id)first;
+
+/**
+ * Answer the first count elements of the receiver. If the receiver contains
+ * less than count elements, answer all of the receiver's elements.
+ */
 - (id)first:(NSUInteger)count;
 
 @end

4a9f7726f8cd0f4928198e3946ff6415b3606958
Leo Schweizer <leoschweizer@users.noreply.github.com>
2015-08-06 18:41:40 +0200

    added error macro

--- Lib/OpinionatedMacros.h
+++ Lib/OpinionatedMacros.h
@@ -1,9 +1,10 @@
 
-#define NotYetImplemented() @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s is not implemented yet", __PRETTY_FUNCTION__] userInfo:nil]
+#define NotYetImplemented (@throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s is not implemented yet", __PRETTY_FUNCTION__] userInfo:nil])
 
-#define SubclassResponsibility() @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s should be overridden in a subclass", __PRETTY_FUNCTION__] userInfo:nil]
+#define SubclassResponsibility (@throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s should be overridden in a subclass", __PRETTY_FUNCTION__] userInfo:nil])
 
-#define ShouldNotOccur() @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s should not be called", __PRETTY_FUNCTION__] userInfo:nil]
+#define ShouldNotOccur (@throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s should not be called", __PRETTY_FUNCTION__] userInfo:nil])
 
-#define UnsupportedOperation() @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s is not supported", __PRETTY_FUNCTION__] userInfo:nil]
+#define UnsupportedOperation (@throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s is not supported", __PRETTY_FUNCTION__] userInfo:nil])
 
+#define Error(message) (@throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%s - %s", __PRETTY_FUNCTION__, message] userInfo:nil])

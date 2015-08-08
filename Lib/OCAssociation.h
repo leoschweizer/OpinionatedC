871f3773b71fea8111fba9d7b9776ec4d5fe3dbb
Leo Schweizer <leoschweizer@users.noreply.github.com>
2015-08-08 23:20:00 +0200

    documentation [ci skip]

--- Lib/OCAssociation.h
+++ Lib/OCAssociation.h
@@ -1,16 +1,36 @@
 #import <Foundation/Foundation.h>
 
 
+/**
+ * An Association is a pair of a key and a value.
+ * It can serve as an entry in a dictionary.
+ */
 @interface OCAssociation : NSObject
 
+/**
+ * Answer the lookup key of the receiver.
+ */
 @property (nonatomic, readonly) id key;
+
+/**
+ * Answer the value of the receiver.
+ */
 @property (nonatomic, readonly) id value;
 
+/**
+ * Answer a new instance of the receiver with the arguments as the key and
+ * value of the association.
+ */
 - (instancetype)initWithKey:(id)key value:(id)value;
 
 @end
 
 
+/**
+ * The category OCAssociationSupport offers convenience initializers for
+ * OCAssociation instances. These should be used in favor of the OCAssociation
+ * initializer.
+ */
 @interface NSObject (OCAssociationSupport)
 
 /**

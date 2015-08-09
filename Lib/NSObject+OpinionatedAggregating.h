c017d8c85ba11f86c5c4c353b3af570536f9b70d
Leo Schweizer <leoschweizer@users.noreply.github.com>
2015-08-09 19:18:23 +0200

    language consistency

--- Lib/NSObject+OpinionatedAggregating.h
+++ Lib/NSObject+OpinionatedAggregating.h
@@ -28,7 +28,7 @@ typedef id (^OCGroupByBlock)(id each);
 
 /**
  * Return a dictionary whose keys are the result of evaluating groupedByBlock
- * for all of the receiver's elements. The value for each key is the collection
+ * for each of the receiver's elements. The value for each key is the collection
  * of elements that evaluated to that key.
  */
 - (NSDictionary *)groupedBy:(OCGroupByBlock)groupedByBlock;

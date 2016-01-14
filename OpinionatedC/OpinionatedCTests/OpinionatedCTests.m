947096a9bbae201019ed89f758e787528a24dae9
Leo Schweizer <leoschweizer@users.noreply.github.com>
2016-01-14 19:14:00 +0100

    carthage support refactoring

---
+++ OpinionatedC/OpinionatedCTests/OpinionatedCTests.m
@@ -0,0 +1,39 @@
+//
+//  OpinionatedCTests.m
+//  OpinionatedCTests
+//
+//  Created by Leo Schweizer on 14/01/16.
+//  Copyright © 2016 Leo Schweizer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OpinionatedCTests : XCTestCase
+
+@end
+
+@implementation OpinionatedCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
